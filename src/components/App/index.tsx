c73d994314fedf06e579fdd4cca5658e60f53e44
Maxim Sklyarov <sklyarov.ma@gmail.com>
2022-06-03 12:46:59 +0300

    App remarks

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// TODO: Remove unused import
 import logo from '../../logo.svg';
 import './App.css';
 import MainApp from '../MainApp';
@@ -7,23 +8,33 @@ import {
 } from 'react-redux';
 
 function App() {
+    // TODO: Use Todo[] type instead of any[]
     const todos = useSelector((state: {list: { todos: any[] }}) => state.list.todos);
   return (
+    // TODO: Obvious, meaningless comment, it doesn't clarify anything
+    // also it uses Ruglish words (English in Russian letters), even if we allow comments in Russian
+    // better to use formal style using English words to specify something
+    // Suggest to remove it
       // туду лист для юзеров:
     <div className="App main">
       <header className="App-header">
         TODO list with users:
+        {/* TODO: remove tag below if we don't need it */}
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
       </header>
+        {/* TODO: Meaninless comment, better to remove */}
         {/* MAIN APP: */}
+        {/* TODO: Create ErrorBoundary HOC to process rendering errors via componentDidCatch */}
         <MainApp todos={todos}/>
 
+        {/* TODO: Replace link URL with correct address */}
         <footer className='App-footer'>
               <a
                 href="https://example.org"
                 target="_blank"
                 className={"App-footer-link"}
               >
+                {/* TODO: Must be "All rights reserved */}
                 All right reserved
               </a>
         </footer>
